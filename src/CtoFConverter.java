f42729bc16ebcbde6281c1deb7b83bc3f4deb70e
reyesmma <reyesmma@mail.uc.edu>
2026-02-23 18:23:50 -0500

    Task 01 Completed

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 
-void main()
+public class CtoFConverter
 {
+    public static void main(String[] args)
+    {
+        double degC = 0.0;
+        double degF;
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        String trash = "";
 
+        do
+        {
+            System.out.print("Enter temperature in degrees Celsius: ");
+            if(in.hasNextDouble())
+            {
+                degC = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Invalid input: " + trash + "\n" + "Enter a valid number.");
+
+            }
+        }
+        while(!done);
+
+        degF = (degC * 9/5) + 32;
+        System.out.println(degC + " degrees Celsius is " + degF + " degrees Fahrenheit.");
+    }
 }
